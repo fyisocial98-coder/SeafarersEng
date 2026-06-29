09e760d06b2a5d96be3f991abc57da69cb3a5741
fyisocial98-coder <fyisocial98@gmail.com>
2026-06-29 14:50:54 +0630

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+<script>
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAY1l0guU0jM4qLvbxuiodiigLGFmTvNYc",
+  authDomain: "meptenglish.firebaseapp.com",
+  databaseURL: "https://meptenglish-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "meptenglish",
+  storageBucket: "meptenglish.firebasestorage.app",
+  messagingSenderId: "84031528665",
+  appId: "1:84031528665:web:3c9d30491961499e11e8c8",
+  measurementId: "G-4VQDD11S5S"
+};
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.database();
+</script>
